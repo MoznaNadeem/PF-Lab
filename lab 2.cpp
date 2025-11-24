545244203d30cef4f1d637696a7f01cd9577a446
Mozna <f2025-0440@bnu.edu.pk>
2025-11-24 21:53:30 +0500

    Add files via upload

---
+++ lab 2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int farenheit;
+	int celsius;
+	cout<<"temp in celsius"<<endl;
+	cin>>celsius;
+	farenheit=(celsius*9/5)+32;
+	cout<<farenheit<<endl;
+	return 0;
+}
